dd46f63bc05160e9d984f9e8e5bdf47e13fbc47f
Laith <76237837+laithaustin@users.noreply.github.com>
2025-06-04 10:46:42 -0500

    Add interpreter integration test

--- tests/mod.rs
+++ tests/mod.rs
@@ -2,8 +2,10 @@ mod scanner;
 mod common;
 mod parser;
 mod functions;
+mod interpreter;
 
 pub use scanner::*;
 pub use common::*;
 pub use parser::*;
 pub use functions::*;
+pub use interpreter::*;
